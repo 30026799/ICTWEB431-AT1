37e90b0bbb5bc6d87c405ef761463d106c85dcef
30026799 <30026799@students.sunitafe.edu.au>
2025-04-23 14:58:31 +1000

    Add files via upload

---
+++ LoggedinScripts.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    var x = document.getElementById("myText").value;
+    var demo = document.getElementById("demo").value;
+
+    document.getElementById("demo").value = x;
+}
+function ButtonHold() {
+        document.getElementById("LogBut").style.backgroundColorcolor = "#04AA6D";
+}
